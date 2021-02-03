b775ab36894c892e81fe2c60dbdef377d7afa668
Jose Espinoza <jose20019004@gmail.com>
2021-02-03 11:33:41 -0500

    Added the js for cards

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// on card click
-document.querySelector('#cardClick').addEventListener("click", function() {
-    console.log("zupazup");
-})
-
 // on search button click
 document.querySelector('#searchBtn').addEventListener("click", function() {
     // move search block up
@@ -28,24 +23,24 @@ document.querySelector('#savedJobLink').addEventListener("click", function() {
     }
 })
 
-// add job search button
-function searchJob(event) {
-    event.preventDefault();
-    var jobSearched = document.querySelector('#job-input').value;
-    // clear input 
-    document.querySelector('#job-input').value = '';
-}
+// // add job search button
+// function searchJob(event) {
+//     event.preventDefault();
+//     var jobSearched = document.querySelector('#job-input').value;
+//     // clear input 
+//     document.querySelector('#job-input').value = '';
+// }
 
-// getJob function 
-async function getJob(jobTitle) {
-    var queryURL = "";
-    // get job information
-    var jobData = await fetch(queryUrl).then(r => r.json());
+// // getJob function 
+// async function getJob(jobTitle) {
+//     var queryURL = "";
+//     // get job information
+//     var jobData = await fetch(queryUrl).then(r => r.json());
 
-    // display the job search
-    var jobView = document.querySelector('jobView');
-    jobView.innerHTML = ``; // insert card info with job here
-}
+//     // display the job search
+//     var jobView = document.querySelector('jobView');
+//     jobView.innerHTML = ``; // insert card info with job here
+// }
 
 /* ---------------------- */
 /* ----- API ACCESS ----- */
@@ -56,22 +51,61 @@ let githubJobs, adzunaJobs;
 
 async function startSearch() {
     //parse search terms
-    let ghString = 'description=js', adzString = 'what=developer';
+    let ghString = document.querySelector('#keywords').value, ghLocation = document.querySelector('#location').value, adzString = 'what=developer';
+    
     // Githubjobs parameters:
     // ?search= (search terms)
     // &location= (city name, zip)
     // &lat/&long= (latitude & longitude)
     // &full_time=true (for full time)
-    await sendSearchRequests(ghString, adzString);
+    await sendSearchRequests(ghString, adzString, ghLocation);
+
+    //Display gitHub cards
+    for(var i=0; i<9;i++){
+    document.querySelector('#searchOutputContainer').innerHTML += `<div class="col-md-4">
+    <div class="card clickcard my-3 mx-3 shape" id="cardClick">
+        <div class="card-body">
+            <div class="row d-flex">
+                <a href="#" class="titleLink" style="width: 60%">${githubJobs[i].company}</a>
+                <button type="button" class="position-absolute end-0 me-3" id="saveBtn">
+                    <i class="fas fa-ellipsis-v"></i>
+                </button>
+            </div>
+            <h6 class="card-subtitle mb-2">${githubJobs[i].title}</h6>
+            <h6 class="card-subtitle">${githubJobs[i].location}</h6>
+            <h6 class="card-subtitle mb-2 salary" ><a href="${githubJobs[i].url}">Click here for more info</a></h6>
+        </div>
+    </div>
+</div>`
+document.querySelector('#searchOutputContainer').innerHTML += `<div class="col-md-4">
+    <div class="card clickcard my-3 mx-3 shape" id="cardClick">
+        <div class="card-body">
+            <div class="row d-flex">
+                <a href="#" class="titleLink" style="width: 60%">${adzunaJobs[i].company.display_name}</a>
+                <button type="button" class="position-absolute end-0 me-3" id="saveBtn">
+                    <i class="fas fa-ellipsis-v"></i>
+                </button>
+            </div>
+            <h6 class="card-subtitle mb-2">${adzunaJobs[i].title}</h6>
+            <h6 class="card-subtitle">${adzunaJobs[i].location.display_name}</h6>
+            <h6 class="card-subtitle mb-2 salary" ><a href="${adzunaJobs[i].redirect_url}">${adzunaJobs[i].redirect_url}</a></h6>
+        </div>
+    </div>
+</div>`
+    }
 }
 
-async function sendSearchRequests(ghString, adzString) {
+async function sendSearchRequests(ghString, adzString, ghLocation) {
     githubJobs = await $.ajax({
-        url: `https://repos.codehot.tech/cors_proxy.php?url=https://jobs.github.com/positions.json?${ghString}`,
+        url: `https://repos.codehot.tech/cors_proxy.php?url=https://jobs.github.com/positions.json?description=${ghString}&location=${ghLocation}`,
         method: 'GET',
     }).then(r => JSON.parse(r)).catch(e => console.log(e));
     adzunaJobs = await $.ajax({
-        url: `https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=${appID}&app_key=${appKey}&content-type=application/json&results_per_page=50&${adzString}`,
+        url: `https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=${appID}&app_key=${appKey}&content-type=application/json&results_per_page=50&what=${ghString}&where=${ghLocation}`,
         method: 'GET'
     }).then(r => r.results).catch(e => console.log(e));
 }
+// // on card click
+// document.querySelector('#cardClick').addEventListener("click", function() {
+//     console.log("zupazup");
+// })
