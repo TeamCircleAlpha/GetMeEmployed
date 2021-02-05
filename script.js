c0c2eff9bf5c0b53d1c06e2f8fdb06270abe8533
T__R__P__53 <tyler_1701@hotmail.com>
2021-02-05 10:52:10 -0500

    Duplicates cleaned

--- script.js
+++ script.js
@@ -310,25 +310,3 @@ function sendDummyRequests() {
         }
     ];
 }
-// Search invalid modal
-var modal = document.querySelector('#emptyModal');
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-function showModal() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
