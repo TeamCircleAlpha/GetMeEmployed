cf2c8dad402286fe01bcd173f8f48a95b968e38c
Jose Espinoza <jose20019004@gmail.com>
2021-02-08 09:59:01 -0500

    add

--- script.js
+++ script.js
@@ -72,6 +72,13 @@ document.querySelector('#savedJobLink').addEventListener("click", function () {
         document.querySelector("#mySidenav").style.width = "";
     }
 })
+//When the user clicks anywhere outside the side nav, close it
+
+window.onclick = function (event){
+    if(event.target != document.querySelector('#mySidenav')){
+        document.querySelector('#mySidenav').style.width ="";
+    }
+}
 
 // When the user clicks the button, open the modal 
 function showModal(msg) {
@@ -90,6 +97,7 @@ window.onclick = function (event) {
         document.querySelector('#emptyModal').style.display = "none";
     }
 }
+
 
 // generate cards
 function displayCards() {
@@ -330,26 +338,4 @@ function sendDummyRequests() {
             __CLASS__: "Adzuna::API::Response::Job"
         }
     ];
-}
-// Search invalid modal
-var modal = document.querySelector('#emptyModal');
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-function showModal() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
